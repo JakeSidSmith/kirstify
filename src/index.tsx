11008dcf0801dc5e18a9720d3f4a6c835071b056
Jake 'Sid' Smith <jake@dabapps.com>
2021-03-18 17:59:08 +0000

    Setup basic markup and styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <p>Hello, World!</p>;
+const App = () => (
+  <main>
+    <h1>Kirstify</h1>
+    <p>
+      Hold you e'er wished you could arrange togehter letter condemnation view
+      Kirsty?
+    </p>
+    <p>Forthwith you lavatory!</p>
+    <textarea placeholder="Enter some text to Kirstify..."></textarea>
+    <button>Kirstify!</button>
+    <div className="output"></div>
+    <blockquote>
+      <p>
+        So casually he presenteth me with what I can only assess as to be one
+        of, if not the uppermost defining moments of my heretowith life.
+      </p>
+      <p>I feel seen.</p>
+      <footer>- Kirsty</footer>
+    </blockquote>
+  </main>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
